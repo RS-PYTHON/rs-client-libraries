704cbb05a976af58130473f92a42eedf7081536a
Alex Grosu <agrosu@c-s.ro>
2024-02-19 16:56:18 +0200

    Modify to pytest

--- rs_workflows/example.py
+++ rs_workflows/example.py
@@ -1,5 +1,6 @@
 from prefect import flow, task
 
+
 @task
 def print_hello(name: str):
     """Example task to be used in a prefect flow
@@ -17,9 +18,10 @@ def print_hello(name: str):
     """
     print(f"Hello {name}!")
 
+
 @flow(name="Hello RS-Server flow ")
 def hello_world(name="COPERNICUS", tasks_number=2):
-     """Example flow that can be use in a COPERNICUS chain
+    """Example flow that can be use in a COPERNICUS chain
 
     This prefect flow may be used as start point in creating your own prefect flows. It runs in parallel
     tasks_number of print_hello
@@ -34,5 +36,5 @@ def hello_world(name="COPERNICUS", tasks_number=2):
     Raises:
         None
     """
-     for idx in range(0, tasks_number):
+    for idx in range(0, tasks_number):
         print_hello(name + "_" + str(idx))
