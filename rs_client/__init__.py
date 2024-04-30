9a96489a4840fd12b70410bc1cc6defed934f292
Alex Grosu <agrosu@c-s.ro>
2024-04-30 16:41:45 +0300

    Add init file for RsClient

---
+++ rs_client/__init__.py
@@ -0,0 +1,9 @@
+"""rsclient main module."""
+
+# Set automatically by running `poetry dynamic-versioning`
+__version__ = "0.0.0"
+
+# Configure OpenTelemetry
+from rs_workflows.utils import opentelemetry
+
+opentelemetry.init_traces("rs.client")
