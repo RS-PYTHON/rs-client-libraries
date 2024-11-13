33ac6371cbfdf533fe73dc3de95a74b2ab0e4b25
pcuq-ads <87175027+pcuq-ads@users.noreply.github.com>
2024-11-13 17:04:45 +0100

    Update logging.py to expose trace_id
    Update to be compliant to ICD COPRS-ICD-ADST-001919935

--- rs_common/logging.py
+++ rs_common/logging.py
@@ -90,7 +90,11 @@ class CustomFormatter(logging.Formatter):
     _PURPLE = "\x1b[35m"
     _RESET = "\x1b[0m"
 
-    _FORMAT = f"%(asctime)s.%(msecs)03d [{{color}}%(levelname)s{_RESET}] (%(name)s) %(message)s"
+    _FORMAT = (
+    f"%(asctime)s [{{color}}%(levelname)s{_RESET}] [%(name)s] "
+    f"[%(filename)s:%(lineno)d] "
+    f"[trace_id=%(otelTraceID)s span_id=%(otelSpanID)s resource.service.name=%(otelServiceName)s trace_sampled=%(otelTraceSampled)s] - %(message)s"
+    )
     _DATETIME = "%H:%M:%S"
 
     _FORMATS = {
