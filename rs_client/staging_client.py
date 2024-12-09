665a9e5c1a7e9d94e6e29ce54a66669546a3c4f2
mcolinde <matthieu.colin-de-verdiere@csgroup.eu>
2024-12-09 23:28:53 +0100

    feat: use property decorator for endpoint definition

--- rs_client/staging_client.py
+++ rs_client/staging_client.py
@@ -28,7 +28,8 @@ import botocore
 import requests
 from pystac import Collection, Extent, SpatialExtent, TemporalExtent
 
-from rs_client.rs_client import RsClient
+from rs_client.rs_client import TIMEOUT, RsClient
+
 from rs_common.config import ECadipStation
 from rs_common.logging import Logging
 pp = pprint.PrettyPrinter(indent=2, width=80, sort_dicts=False, compact=True)
@@ -38,7 +39,8 @@ if os.getenv("RSPY_LOCAL_MODE") == "1":
 else:
     RSPY_HOST_STAGING = "http://rs-server-staging:8000"
 class StagingClient(RsClient):
-    """Class to handle the staging process in rs-client-libraries
+    """
+    Class to handle the staging process in rs-client-libraries
 
     This class provides python methods to call the different endpoints of the rs-server-staging method
 
@@ -55,58 +57,50 @@ class StagingClient(RsClient):
         super().__init__(rs_server_href, rs_server_api_key, owner_id, logger)
         # Define logger
         self.logger = logger
+        self.resource = "staging" ###
+        self.job_id = None ###
+        self.href_staging = None ###
 
-    def get_processes(self, timeout: int):
+    
+    @property
+    def get_processes(self) -> str:
         """
         Call endpoint /processes - Returns list of all available processes from config
         """
-        return requests.get(f"{RSPY_HOST_STAGING}/processes", timeout=timeout)  # pylint: disable=too-many-arguments
+        return f"{self.href_staging}/processes"
+    
+    @property
+    def get_resources(self) -> str:
+        """
+        Call endpoint /processes - Returns list of all available processes from config
+        """
+        return f"{self.href_staging}/processes/{self.resource}"
+ 
+    @property
+    def execute_process(self) -> str:
+        """
+        Call endpoint /processes - Returns list of all available processes from config
+        """
+        return f"{self.href_staging}/processes/{self.resource}/execution"
 
-    def get_resource(self, resource: str, timeout: int):
+    @property
+    def get_job_status(self) -> str:
         """
-        Call endpoint /processes/{resource} - Should return info about a specific resource
+        Call endpoint /processes - Returns list of all available processes from config
         """
-        return requests.get(
-            f"{RSPY_HOST_STAGING}/processes/{resource}",
-            timeout=timeout,
-        )  # pylint: disable=too-many-arguments
+        return f"{self.href_staging}/jobs/{self.job_id}"
 
-    def execute_process(self, resource: str, timeout: int):
+    @property
+    def get_jobs(self) -> str:
         """
-        Call endpoint/processes/{resource}/execution - execute processing jobs
-        Args:
-            resource (str): name of the process to execute
+        Call endpoint /processes - Returns list of all available processes from config
         """
-        return requests.get(f"{RSPY_HOST_STAGING}/processes/{resource}/execution", timeout=timeout)
+        return f"{self.href_staging}/jobs/{self.job_id}"
 
-    def get_job_status(self, job_id: int, timeout: int):
-        """
-        Call endpoint /jobs/{job_id} - get status of processing job
-        Args:
-            job_id (int): job identifier
-        """
-        return requests.get(f"{RSPY_HOST_STAGING}/jobs/{job_id}", timeout=timeout)
 
-    def get_jobs(self, timeout: int):
-        """
-        Call endpoint /jobs to get the status of all jobs
-        """
-        return requests.get(f"{RSPY_HOST_STAGING}/jobs", timeout=timeout)
-
-    def delete_job(self, job_id: int, timeout: int):
-        """
-        Call endpoint /jobs/{job_id} - get status of processing job
-        Args:
-            job_id (int): job identifier
-        """
-        return requests.delete(f"{RSPY_HOST_STAGING}/jobs/{job_id}", timeout=timeout)
-
-    def get_specific_job_result(self, job_id):
-        """
-        Call endpoint /jobs/{job_id}/results - get result from a specific job
-        Args:
-            job_id (int): job identifier
-        """
+    ############################
+    # Call RS-Server endpoints #
+    ############################
 
     def run_staging(  # pylint: disable=too-many-locals
         self,
@@ -168,18 +162,13 @@ class StagingClient(RsClient):
         }
 
         # ----- Step 3: Launch and monitor the staging process
-
-        # Remark: this class don't inherits from the owslib.ogcapi.processes.Processes class because the latter
-        # doesn't provide wrapping for all endpoints defined in rs-server-staging (it only provides the  /processes
-        # and /processes/{processId}/execution endpoints + it doesn't allow to manage apikey_header parameter which
-        # is passed as an extra argument)
-        post_response = requests.post(
-            f"{RSPY_HOST_STAGING}/processes/staging/execution",
-            json=staging_body,
-            **apikey_headers,
-            timeout=timeout,
-        )
-
+        post_response = self.http_session.post(
+                url=self.execute_process,
+                json=staging_body,
+                timeout=TIMEOUT,
+                **self.apikey_headers,
+            )
+        
         resp = json.loads(post_response.content)
         pprint.PrettyPrinter(indent=4).pprint(resp)
 
@@ -208,107 +197,3 @@ class StagingClient(RsClient):
                 continue
             time.sleep(2)
             timeout -= 2
-
-
-if __name__ == "__main__":
-    # ----------------- Input data -----------------
-    TIMEOUT = 10
-    CATALOG_BUCKET = "rs-cluster-catalog"
-    APIKEY_HEADER = "x-api-key"
-    COLLECTION_ID = "cadip_s1A"
-    STAC_OUTPUT_COLL_NAME = "cadip_s1A_staged"
-    APIKEY_VALUE = None  # "x-api-key" ### TODO: get apikey from frontend page
-    APIKEY_HEADERS: dict = {"headers": {APIKEY_HEADER: APIKEY_VALUE}} if APIKEY_VALUE else {}
-    user = os.getenv("RSPY_HOST_USER", default=getpass.getuser())
-    local_mode = os.getenv("RSPY_LOCAL_MODE") == "1"
-    rs_server_href = "" if local_mode else os.getenv("RSPY_WEBSITE")
-
-    # Get client instances from the generic client
-    generic_client = RsClient(
-        rs_server_href,
-        rs_server_api_key=APIKEY_VALUE,
-        owner_id=user,
-        logger=None,
-    )
-    stac_client = generic_client.get_stac_client()
-    cadip_station = ECadipStation.CADIP  # you can also have: INS, MPS, MTI, NSG, SGS
-    cadip_client = generic_client.get_cadip_client(cadip_station)
-    staging_client = generic_client.get_staging_client()
-
-    logger = Logging.default(__name__)
-
-    # ----- Step 1 - Create the output STAC collection if it doesn't alredy exists
-    logger.info(f"Creating a new collection {STAC_OUTPUT_COLL_NAME} in the STAC catalog...")
-    try:
-        create_coll_response = stac_client.get_collection(STAC_OUTPUT_COLL_NAME)
-        logger.info(
-            f"Collection {STAC_OUTPUT_COLL_NAME} already exists -> staging process will use the existing one",
-        )
-    except: # pylint: disable=bare-except
-        create_coll_response = stac_client.add_collection(
-            Collection(
-                id=STAC_OUTPUT_COLL_NAME,
-                description=None,  # rs-client will provide a default description for us
-                extent=Extent(
-                    spatial=SpatialExtent(bboxes=[-180.0, -90.0, 180.0, 90.0]),
-                    temporal=TemporalExtent(
-                        [datetime(2000, 1, 1), datetime(2030, 1, 1)],
-                    ),
-                ),
-            ),
-        )
-        logger.info(
-            f"Resp status: {create_coll_response.status_code} | Message: {create_coll_response.reason}",
-        )
-
-    # ----- Step 2 - Create a bucket to store staged data
-    PREFIX = "stream/"
-    s3_session = boto3.session.Session()
-    s3_client = s3_session.client(
-        service_name="s3",
-        aws_access_key_id=os.environ["S3_ACCESSKEY"],
-        aws_secret_access_key=os.environ["S3_SECRETKEY"],
-        endpoint_url=os.environ["S3_ENDPOINT"],
-        region_name=os.environ["S3_REGION"],
-    )
-    try:
-        s3_client.head_bucket(Bucket=CATALOG_BUCKET)
-        logger.info(f"The bucket {CATALOG_BUCKET} already exists")
-    except botocore.client.ClientError as error:
-        if int(error.response["Error"]["Code"]) == 404:
-            try:
-                s3_client.create_bucket(Bucket=CATALOG_BUCKET)
-            except botocore.exceptions.ClientError as e:
-                logger.info(f"Bucket CATALOG_BUCKET error: {e}")
-                sys.exit(-1)
-        else:
-            logger.info("PANIC: Could not get bucket info. Exiting")
-            sys.exit(-1)
-    # Delete all existing objects from rs-server-catalog
-    if PREFIX:
-        response = s3_client.list_objects_v2(Bucket=CATALOG_BUCKET, Prefix=PREFIX)
-        if response.get("Contents", None):
-            for elem in response["Contents"]:
-                logger.info(f"Deleting {elem['Key']}")
-                s3_client.delete_object(Bucket=CATALOG_BUCKET, Key=elem["Key"])
-
-    # Apply a request to get information about sessions that you want to stage
-    session = requests.Session()
-    search_result = session.get(f"{os.getenv('RSPY_HOST_CADIP')}/cadip/collections/{COLLECTION_ID}/items").json()
-    
-    # Launch staging process
-    staging_client.run_staging(APIKEY_HEADERS, search_result, STAC_OUTPUT_COLL_NAME, TIMEOUT)
-
-    # Check created sessions 
-    result = session.get(f"{os.getenv('RSPY_HOST_CATALOG')}/catalog/collections/{STAC_OUTPUT_COLL_NAME}/items")
-    catalog_collection = result.json()
-    assert catalog_collection.get("type") == "FeatureCollection"
-    assert len(catalog_collection.get("features")) == 4
-    for item in catalog_collection.get("features"):
-        print(f"Item {item.get('id')} has {len(item.get('assets'))} assets") 
-    
-    # Delete the whole collection
-    # logger.info("Deleting the collection...")
-    # result = session.delete(f"{os.getenv('RSPY_HOST_CATALOG')}/catalog/collections/{STAC_OUTPUT_COLL_NAME}")
-    # assert result.json()["deleted collection"] == STAC_OUTPUT_COLL_NAME
-    # pp.pprint(result.json())
