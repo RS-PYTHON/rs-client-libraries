f9732df9ab4f73a6567d5990cfe524d3fa33d735
Etienne Combelles <etienne.combelles@cs-soprasteria.com>
2025-10-14 14:07:38 +0200

    RSPY-798: Linter fixes

--- rs_workflows/auxip_flow.py
+++ rs_workflows/auxip_flow.py
@@ -19,7 +19,7 @@ import json
 
 from prefect import flow, get_run_logger, task
 from prefect.artifacts import acreate_table_artifact
-from pystac import Item, ItemCollection
+from pystac import ItemCollection
 
 from rs_client.stac.auxip_client import AuxipClient
 from rs_workflows.catalog_flow import catalog_search_task
@@ -92,7 +92,7 @@ async def auxip_staging(
     with flow_env.start_span(__name__, "auxip-staging"):
 
         # Search Auxip products
-        auxip_items = (
+        _any, _any, auxip_items = (
             search_task.with_options(timeout_seconds=timeout_seconds if timeout_seconds >= 0 else None)
             .submit(
                 flow_env.serialize(),
@@ -108,7 +108,7 @@ async def auxip_staging(
             return True, None
 
         # Search catalog items
-        catalog_items = catalog_search_task.submit(
+        _any, _any, catalog_items = catalog_search_task.submit(
             flow_env.serialize(),
             catalog_cql2=cql2_filter,
             error_if_empty=False,
@@ -140,7 +140,7 @@ async def auxip_staging(
 
         # Wait for last task to end.
         # NOTE: use .result() and not .wait() to unwrap and propagate exceptions, if any.
-        staging_results = staged.result()
+        _any, _any, staging_results = staged.result()
 
         # Check that all jobs monitored were successful. Otherwise, return status is "False"
         return_status = True
