c67d9139ed05b838dc78a8b9dfd9bc0cf1c1b556
Alex Grosu <agrosu@c-s.ro>
2026-02-09 15:48:07 +0200

    More tests over ths S03CACHE output inside the payload file

--- rs_workflows/payload_generator.py
+++ rs_workflows/payload_generator.py
@@ -405,7 +405,12 @@ def build_output_products(
         outputs.append(
             OutputProduct(
                 id=mapping["name"],
-                path=output_path,
+                path=(
+                    output_path
+                    if mapping["name"] != "S03CACHE"
+                    else "s3://rs-dev-cluster-temp/prefect-share"
+                    "/users/agrosu/output/l0/s3.short/S3_D_TDS_1/output/default/CACHE.short"
+                ),
                 store_type=mapping["store_type"],
                 store_params=storage_configuration.get_store_params(store_name),
                 type=mapping.get("type", "filename"),
