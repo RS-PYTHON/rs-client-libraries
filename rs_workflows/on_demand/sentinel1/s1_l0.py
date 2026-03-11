bca9b30b985543d9ab6a9c66b7131a5ff053d978
pcuq-ads <pierre.cuq@airbus.com>
2026-03-11 16:45:20 +0100

    Add Cadip session search functionality and improve session retrieval logging

--- rs_workflows/on_demand/sentinel1/s1_l0.py
+++ rs_workflows/on_demand/sentinel1/s1_l0.py
@@ -21,6 +21,9 @@ from pystac import Item, ItemCollection
 
 from rs_client.stac.catalog_client import CatalogClient
 from rs_workflows.flow_utils import FlowEnv, FlowEnvArgs
+from rs_workflows.on_demand.stage_last_sessions import stage_session_common, cadip_session_search
+from rs_workflows.utils.artifact_verbose import ReportManager
+from rs_client.stac.cadip_client import CadipClient
 
 
 @flow(name="process a sentinel-1 sessions")
@@ -42,16 +45,44 @@ async def s1l0_processing(
     logger.info("Sentinel-1 session name is correct. ")
 
     flow_env = FlowEnv(FlowEnvArgs(owner_id=owner_identifier))
-    with flow_env.start_span(__name__, "cadip-search"):
+    with flow_env.start_span(__name__, "sentinel1-level0"):
         catalog_client: CatalogClient = flow_env.rs_client.get_catalog_client()
 
         # Try to retrieve the session on the collection
         logger.info("Search session on the rs-catalog.")
         item_collection = catalog_client.search(method="POST", collections=[collection], ids=[session])
-        count = len(item_collection.items)
-        if count == 1:
-            print(f"The session '{session}' has been found on the collection '{collection}'.")
+        if item_collection is not None:
+            count = len(item_collection.items)
         else:
-            print(f"The session '{session}' has NOT been found on the collection '{collection}'.")
+            count = 0
+        if count == 1:
+            print(f"The session '{session}' has been found on the rs-catalog collection '{collection}'.")
+        else:
+            print(f"The session '{session}' has NOT been found on the rs-catalog collection '{collection}'.")
             print("Try to stage it from all S1 stations.")
-        # print(json.dumps(item_collection.to_dict(), indent=2))
+            item_col = cadip_session_search_by_name(flow_env, session)
+            print(json.dumps(item_col.items[0].to_dict(), indent=2))
+
+
+@task(name="Cadip session search by name")
+async def cadip_session_search_by_name(env: FlowEnv, session: str) -> ItemCollection:
+    """
+    """
+    logger = get_run_logger()
+
+    # Initialize flow environment and telemetry span
+    cadip_client: CadipClient = env.rs_client.get_cadip_client()
+
+    # Log query for debugging
+    logger.info("Start request on all S1 CADIP stations")
+
+    # Execute search request
+    found = cadip_client.search(
+        method="GET",
+        ids=[session],
+        collections=["s1_ins", "s1_kse", "s1_mps", "s1_mti", "s1_nsg", "s1_sgs"],
+        max_items=1,
+        limit=1
+    )
+
+    return found
