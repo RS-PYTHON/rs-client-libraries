0a71e9133fe8de1f0e8f2109b31e1cea40dbd6a6
pcuq-ads <pierre.cuq@airbus.com>
2026-03-19 14:03:50 +0100

    test

--- rs_workflows/on_demand/common/l0.py
+++ rs_workflows/on_demand/common/l0.py
@@ -52,7 +52,7 @@ class GeneratedProductTEST(BaseModel):
 
     name: str = Field(description="Output product name.")
     product_type: str = Field(description="Product type.")
-    collection_name: str = Field(
+    collection_name: str | None = Field(
         description="Collection name. If not provided, it defaults to product_type.",
     )
     #collection_name: str | None = Field(
