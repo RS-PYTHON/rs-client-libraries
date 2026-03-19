3761123b0a5bb7d34657e0235a9ed7947db86dc3
pcuq-ads <pierre.cuq@airbus.com>
2026-03-19 11:44:58 +0100

    test

--- rs_workflows/on_demand/common/l0.py
+++ rs_workflows/on_demand/common/l0.py
@@ -111,8 +111,20 @@ class TestModel(BaseModel):
         title="CADIP Collections",
         description="List of CADIP collections to query for session retrieval."
     )
-    generated_product_to_collection_identifier: list[GeneratedProduct] = Field(
+    generated_product_to_collection_identifier: List[GeneratedProduct] = Field(
         default_factory=list,
+        default=[
+            {
+                "name": "S03ISPS",
+                "product_type": "*",
+                "collection_name": "s3_production_isps_s3l0"
+                },
+            {
+                "name": "S03CACHE",
+                "product_type": "*",
+                "collection_name": "s3_production_cache_s3l0"
+                }
+            ],
         title="Generated Product Mapping",
         description="List of generated products and their target collections."
     )
