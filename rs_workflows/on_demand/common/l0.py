d97a022503ba5938d26302bfb8558ef1be22fabd
pcuq-ads <pierre.cuq@airbus.com>
2026-03-19 10:58:45 +0100

    test

--- rs_workflows/on_demand/common/l0.py
+++ rs_workflows/on_demand/common/l0.py
@@ -106,7 +106,13 @@ class TestModel(BaseModel):
         title="Workflow Type",
         description="Workflow type to execute (on-demand, scheduled, etc.)."
     )
-    
+    generated_product_to_collection_identifier: list[GeneratedProduct] = Field(
+        title="Generated Products",
+        description=(
+            "List of generated products. Each item specifies a name, the product type, "
+            "and the collection where the output will be stored."
+        )
+    )
 
 
 
