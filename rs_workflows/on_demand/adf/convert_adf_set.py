b92455540ef6346de7058401d6a79afffbaf2ad9
pcuq-ads <pierre.cuq@airbus.com>
2026-06-08 11:42:42 +0200

    fix: correct path for CQL2 filters configuration

--- rs_workflows/on_demand/adf/convert_adf_set.py
+++ rs_workflows/on_demand/adf/convert_adf_set.py
@@ -29,7 +29,7 @@ from prefect.variables import Variable
 from rs_workflows.adf_flow import adf_conversion_task
 from rs_workflows.flow_utils import AdfProcessIn, AuxiliaryProductMapping, FlowEnvArgs
 
-CQL2_FILTERS_PATH: str = "./config/cql2_filters.json"
+CQL2_FILTERS_PATH: str = "./config/cql2-queries.json"
 PREFECT_WORKPOOL: str = "on-demand-k8s-pool-prefect"
 GITHUB_URL: str = "https://github.com/RS-PYTHON/rs-client-libraries.git"
 GITHUB_BRANCH: str = "rspy-1074/create-flow-convert-adf-group"
