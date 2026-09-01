5185b7b6ea61c5d2b81fd5d1dd681aa0fe11c80c
Etienne Combelles <etienne.combelles@cs-soprasteria.com>
2026-09-01 17:27:07 +0200

    RSPY-1103: Try new change

--- rs_workflows/flow_utils.py
+++ rs_workflows/flow_utils.py
@@ -192,8 +192,7 @@ class FlowEnv:
         logging.getLogger("prefect.task_runs").setLevel(args.logging_level.value)
 
         # Set logging level for current run
-        run_logger = get_run_logger()  # type: ignore
-        run_logger.setLevel(args.logging_level.value)
+        logger.setLevel(args.logging_level.value)
 
         # Deserialize the calling span, if any
         if args.calling_span:
@@ -210,7 +209,7 @@ class FlowEnv:
             rs_server_href=os.getenv("RSPY_WEBSITE"),
             rs_server_api_key=os.getenv("RSPY_APIKEY"),
             owner_id=self.owner_id,
-            logger=run_logger,  # type: ignore
+            logger=logger,  # type: ignore
         )
 
     def serialize(self) -> FlowEnvArgs:
